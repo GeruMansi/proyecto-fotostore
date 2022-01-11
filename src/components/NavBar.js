1db6b967937fce35a236a06e3909bbd21764c511
GeruMansi <mangeragu@gmail.com>
2022-01-11 15:14:30 -0300

    agregado componente NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Categorías</a></li>
+                <li><a href="#">Perfil</a></li>
+            </ul>
+        </nav>
+    )
+}
